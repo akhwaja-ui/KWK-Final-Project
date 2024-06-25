5729796d54beee8d4ed008f7d2ce98c1bd3271a7
Aria Khwaja <ariakhwaja11@gmail.com>
2024-06-25 13:02:09 -0400

    Initial Commit

---
+++ KWK-Final Project/KWK_Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  KWK_Final_ProjectApp.swift
+//  KWK-Final Project
+//
+//  Created by Aria Khwaja on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct KWK_Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
