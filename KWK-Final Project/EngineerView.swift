9a03c9cdc3946ed3ad552ac47523ec3a8f87ab87
Aria Khwaja <ariakhwaja11@gmail.com>
2024-06-27 13:29:44 -0400

    push

--- KWK-Final Project/EngineerView.swift
+++ KWK-Final Project/EngineerView.swift
@@ -14,60 +14,116 @@ struct EngineerView: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
             // First Disclosure Group
-            DisclosureGroup("Section 1", isExpanded: $isExpanded1) {
+            DisclosureGroup("SeaPerch", isExpanded: $isExpanded1) {
                 VStack(alignment: .leading, spacing: 10) {
                     // Add your content here
-                    Text("Detailed information for section 1.")
+                    Text("Overview")
+                        .fontWeight(.bold)
+                    
+                    Text("SeaPerch is an innovative underwater robotics program that equips students, educators, and parents with the resources they need to build an underwater remotely operated vehicle (ROV) in an in- or out-of-school setting. As an integrated STEM education program, SeaPerch is a great place to start on your journey to build a bot and learn how to use that bot for good.")
                         .padding()
                         .background(Color.yellow.opacity(0.3))
                         .cornerRadius(5)
                     
-                    // Example image
-                    Image(systemName: "star.fill")
-                        .resizable()
-                        .frame(width: 50, height: 50)
-                        .foregroundColor(.yellow)
-                }
-                .padding()
-                .background(Color.blue.opacity(0.1))
-                .cornerRadius(8)
-            }
-            .padding()
-            .background(Color.blue.opacity(0.3))
-            .cornerRadius(10)
-            
-            // Second Disclosure Group
-            DisclosureGroup("Section 2", isExpanded: $isExpanded2) {
-                VStack(alignment: .leading, spacing: 10) {
-                    // Add your content here
-                    Text("Detailed information for section 2.")
+                    Text("Cost")
+                        .fontWeight(.bold)
+                    Text("Participation is free! Participants are required to bring their own supplies and equipment.")
+                        .fontWeight(.light)
                         .padding()
-                        .background(Color.green.opacity(0.3))
-                        .cornerRadius(5)
+                        .background(Color.blue.opacity(0.3))
                     
-                    // Example image
-                    Image(systemName: "heart.fill")
-                        .resizable()
-                        .frame(width: 50, height: 50)
-                        .foregroundColor(.red)
+                    Text("Who")
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    
+                    Text("Available for all students from 4th to 12th grade!")
+                        .fontWeight(.light)
+                        .padding()
+                        .background(Color.blue.opacity(0.3))
+                    
+                    Text("Requirements")
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    
+                    VStack {
+                        Text("To benefit from the curriculum at Mathcamp, applicants need to be fully comfortable with high-school algebra, geometry, trig, exponents and logarithms. We expect all students to have completed Precalculus (or the equivalent). Calculus is not required (but it's nice to have, and many of our students have already studied it)")
+                            .fontWeight(.light)
+                            .padding()
+                            .background(Color.blue.opacity(0.3))
+                            .cornerRadius(10)
+                        
+                        
+                        Image(systemName: "star.fill")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(.yellow)
+                    }
+                    .padding()
+                    .background(Color.blue.opacity(0.1))
+                    .cornerRadius(8)
                 }
                 .padding()
-                .background(Color.green.opacity(0.1))
-                .cornerRadius(8)
+                .background(Color.blue.opacity(0.3))
+                .cornerRadius(10)
+                
+                // Second Disclosure Group
+                DisclosureGroup("InspiritAI", isExpanded: $isExpanded2) {
+                    VStack(alignment: .leading, spacing: 10) {
+                        // Add your content here
+                        Text("Overview")
+                            .fontWeight(.bold)
+                        
+                        Text("Inspirit AI offers an AI program for all High School students with a strong passion for engineering. With mentors from Stanford and MIT, students are able to collaborate and create AI passion projects with the topic of their choice.")
+                            .padding()
+                            .background(Color.green.opacity(0.3))
+                            .cornerRadius(5)
+                        
+                        Text("Cost")
+                            .fontWeight(.bold)
+                        Text("Summer 2024 cohort pricing is $1200 USD. No application fee required!")
+                            .padding()
+                            .background(Color.green.opacity(0.3))
+                            .cornerRadius(5)
+                        
+                        Text("Who")
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        Text("Available for all Middle-School and High-School Students!")
+                            .padding()
+                            .background(Color.green.opacity(0.3))
+                            .cornerRadius(5)
+                        
+                        Text("Requirements")
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        VStack {
+                            Text("Application with two short-answer questions are required" )
+                                .fontWeight(.light)
+                                .padding()
+                                .background(Color.blue.opacity(0.3))
+                                .cornerRadius(10)
+                            
+                            
+                            // Example image
+                            Image(systemName: "heart.fill")
+                                .resizable()
+                                .frame(width: 50, height: 50)
+                                .foregroundColor(.red)
+                        }
+                        .padding()
+                        .background(Color.green.opacity(0.1))
+                        .cornerRadius(8)
+                    }
+                    .padding()
+                    .background(Color.green.opacity(0.3))
+                    .cornerRadius(10)
+                    
+                    Spacer()
+                }
+                .padding()
             }
-            .padding()
-            .background(Color.green.opacity(0.3))
-            .cornerRadius(10)
-            
-            Spacer()
         }
-        .padding()
+        
+        struct EngineerView_Previews: PreviewProvider {
+            static var previews: some View {
+                EngineerView()
+            }
+        }
+        
     }
-}
-
-struct EngineerView_Previews: PreviewProvider {
-    static var previews: some View {
-        EngineerView()
-    }
-}
-
--- KWK-Final Project/MathView.swift
+++ KWK-Final Project/MathView.swift
@@ -13,14 +13,44 @@ struct MathView: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
             // First Disclosure Group
-            DisclosureGroup("Section 1", isExpanded: $isExpanded1) {
+            DisclosureGroup("MathCamp", isExpanded: $isExpanded1) {
                 VStack(alignment: .leading, spacing: 10) {
                     // Add your content here
-                    Text("Detailed information for section 1.")
+                    Text("Overview")
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    
+                    Text("A five-week summer program for mathematically talented high school students, offering courses and projects in advanced mathematics.")
                         .padding()
                         .background(Color.yellow.opacity(0.3))
                         .cornerRadius(5)
                     
+                    Text("Cost")
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    Text("between 0 and 5,500$")
+                        .fontWeight(.light)
+                        .padding()
+                        .background(Color.blue.opacity(0.3))
+                        .cornerRadius(10)
+                    
+                    Text("Who")
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    
+                    Text("All High-school and Middle-school students passionate about mathmatics are welcome to join. Students between ages 13 and 18 are eligible. For Mathcamp 2024, your birth date should be between August 4, 2005 and June 30, 2011. ")
+                        .fontWeight(.light)
+                        .padding()
+                        .background(Color.blue.opacity(0.3))
+                        .cornerRadius(10)
+                    
+                    Text("Requirements")
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    
+                    VStack {
+                        Text("To benefit from the curriculum at Mathcamp, applicants need to be fully comfortable with high-school algebra, geometry, trig, exponents and logarithms. We expect all students to have completed Precalculus (or the equivalent). Calculus is not required (but it's nice to have, and many of our students have already studied it)")
+                            .fontWeight(.light)
+                            .padding()
+                            .background(Color.blue.opacity(0.3))
+                            .cornerRadius(10)
+                    }
                     // Example image
                     Image(systemName: "star.fill")
                         .resizable()
@@ -36,13 +66,39 @@ struct MathView: View {
             .cornerRadius(10)
             
             // Second Disclosure Group
-            DisclosureGroup("Section 2", isExpanded: $isExpanded2) {
+            DisclosureGroup("Texas Mathworks Honors Summer Math Camp", isExpanded: $isExpanded2) {
                 VStack(alignment: .leading, spacing: 10) {
                     // Add your content here
-                    Text("Detailed information for section 2.")
+                    Text("Overview")
+                        .fontWeight(.bold)
+                    Text("A residential program at Texas State University that focuses on advanced problem-solving and mathematical theory.")
                         .padding()
                         .background(Color.green.opacity(0.3))
                         .cornerRadius(5)
+                    
+                    Text("Cost")
+                        .fontWeight(.bold)
+                    
+                    Text("The full tuition fee for the six-week program from June 16 - July 27, 2024, $6,000. Spots are only ensured when the student has paid their tuition in full. Scholorships are available to students based on financial need")
+                        .padding()
+                        .background(Color.green.opacity(0.3))
+                        .cornerRadius(5)
+                    
+                    Text("Who")
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    Text("All High-School students")
+                        .padding()
+                        .background(Color.green.opacity(0.3))
+                        .cornerRadius(5)
+                    
+                    Text("Requirements")
+                        .fontWeight(.bold)
+                    
+                    Text("Application with Essay and Teacher recommendation is required")
+                        .fontWeight(.light)
+                        .padding()
+                        .background(Color.blue.opacity(0.3))
+                        .cornerRadius(10)
                     
                     // Example image
                     Image(systemName: "heart.fill")
