b01a656a143a72f83aae2bc64b9b418c33b7904d
Meja Santos <mejasantos23@gmail.com>
2024-06-27 20:07:16 -0400

    I fixed the Fourth Disclosure Group. This should fix the issue!

--- KWK-Final Project/TechView.swift
+++ KWK-Final Project/TechView.swift
@@ -1,9 +1,9 @@
 //
-//  TechView.swift
-//  KWK-Final Project
+//TechView.swift
+//KWK-Final Project
 //
-//  Created by Sruti Narain on 6/26/24.
-//
+//Created by Sruti Narain on 6/26/24.
+
 
 import SwiftUI
 
@@ -16,17 +16,17 @@ struct TechView: View {
     @State private var isExpanded5: Bool = false
 
     var body: some View {
-        ScrollView{
+        ScrollView {
             VStack(alignment: .leading, spacing: 10) {
                 Text("Computer Science/Tech Programs")
+                    .font(.largeTitle)
                     .fontWeight(.bold)
+                
                 // First Disclosure Group
                 DisclosureGroup("Girls Who Code (GWC)", isExpanded: $isExpanded1) {
                     VStack(alignment: .leading, spacing: 10) {
-                        // Add your content here
                         Text("Overview")
                             .fontWeight(.bold)
-                        
                         Text("It is a national nonprofit organization that aims to close the gender gap in technology and computer science by inspiring more girls to become computer scientists and engineers. GWC offers a variety of programs, including after-school clubs, summer programs, college and career programs, and GirlJams.")
                             .padding()
                             .background(Color.blue.opacity(0.3))
@@ -42,8 +42,7 @@ struct TechView: View {
                         
                         Text("Who")
                             .fontWeight(.bold)
-                        
-                        Text("GWC offers after-school clubs that are meant for girls in grades 3–5 and 6–12 that teach computer science skills like coding basics and mobile app development. They also have the Summer Immersion program that assists highschool girls in learning computer science skills")
+                        Text("GWC offers after-school clubs that are meant for girls in grades 3–5 and 6–12 that teach computer science skills like coding basics and mobile app development. They also have the Summer Immersion program that assists high school girls in learning computer science skills.")
                             .fontWeight(.light)
                             .padding()
                             .background(Color.blue.opacity(0.3))
@@ -51,13 +50,14 @@ struct TechView: View {
                         
                         Text("More Info on Clubs:")
                             .fontWeight(.bold)
-                        VStack{
+                        VStack {
                             Text("After-School Clubs are run by volunteer teachers and facilitators at nonprofit locations like schools, libraries, and community centers. The curriculum includes self-guided modules and highlights famous women in the field. Most clubs meet weekly for 1–2 hours for at least 10 sessions.")
-                            Text("The GWC Summer Immersion Program is a free, seven-week program aimed at closing the gender gap in technology")
+                            Text("The GWC Summer Immersion Program is a free, seven-week program aimed at closing the gender gap in technology.")
                                 .fontWeight(.light)
                                 .padding()
                                 .background(Color.blue.opacity(0.3))
                                 .cornerRadius(10)
+                        }
                     }
                     .padding()
                     .background(Color.blue.opacity(0.1))
@@ -70,13 +70,13 @@ struct TechView: View {
                 // Second Disclosure Group
                 DisclosureGroup("Google’s Computer Science Summer Institute", isExpanded: $isExpanded2) {
                     VStack(alignment: .leading, spacing: 10) {
-                        // Add your content here
                         Text("Overview")
                             .fontWeight(.bold)
-                        
-                        Text("CSSI aims to provide an introduction to computer science for high school level students with a passion for technology. Their curriculum provides an intensive, interactive and hands-on opportunity for students with minimal exposure to CS to learn the basic concepts of programming. This content seeks to inspire the tech leaders and innovators of tomorrow by supporting the study of computer science, software engineering and other closely related subjects.")
+                        Text("CSSI aims to provide an introduction to computer science for high school level students with a passion for technology. Their curriculum provides an intensive, interactive and hands-on opportunity for students with minimal exposure to CS to learn the basic concepts of programming. This content seeks to inspire the tech leaders and innovators of tomorrow by supporting the study of computer science, software engineering, and other closely related subjects.")
+                            .padding()
                             .background(Color.green.opacity(0.3))
-                            .cornerRadius(5)
+                            .cornerRadius(10)
+                        
                         Text("Cost")
                             .fontWeight(.bold)
                         Text("FREE")
@@ -87,15 +87,15 @@ struct TechView: View {
                         
                         Text("Who")
                             .fontWeight(.bold)
-                        
                         Text("High school level students.")
                             .fontWeight(.light)
                             .padding()
                             .background(Color.green.opacity(0.3))
                             .cornerRadius(10)
-                        Text("Requirements:")
+                        
+                        Text("Requirements")
                             .fontWeight(.bold)
-                        Text("There are no stated requirements, but the only thing CSSI searches for are high school students that are truly passionate about technology and can withstand a intense and hands-on oppurtunity for learning the concepts of programming.")
+                        Text("There are no stated requirements, but CSSI searches for high school students that are truly passionate about technology and can withstand an intense and hands-on opportunity for learning the concepts of programming.")
                     }
                     .padding()
                     .background(Color.green.opacity(0.1))
@@ -108,13 +108,13 @@ struct TechView: View {
                 // Third Disclosure Group
                 DisclosureGroup("Kode With Klossy Summer Camps", isExpanded: $isExpanded3) {
                     VStack(alignment: .leading, spacing: 10) {
-                        // Add your content here
                         Text("Overview")
                             .fontWeight(.bold)
-                        
                         Text("Kode With Klossy camps are a series of FREE 2-week coding-intensive camps designed for students of traditionally underrepresented genders in the STEM fields. Their curriculums aim to not only teach students the fundamentals of coding, but spark a lifelong passion for STEM. Scholars will complete the program with a new skill set and an incredible community of inspiring alumni to learn & grow with. You don’t need to know anything about coding to apply! Camp can be held either virtually or in person for web development, mobile app development, machine learning, or data science.")
+                            .padding()
                             .background(Color.mint.opacity(0.3))
                             .cornerRadius(10)
+                        
                         Text("Cost")
                             .fontWeight(.bold)
                         Text("FREE")
@@ -125,16 +125,15 @@ struct TechView: View {
                         
                         Text("Who")
                             .fontWeight(.bold)
-                        
                         Text("Any female, non-binary, or transgender 13-18 years old.")
                             .fontWeight(.light)
                             .padding()
                             .background(Color.mint.opacity(0.3))
                             .cornerRadius(10)
-                        Text("Requirements:")
+                        
+                        Text("Requirements")
                             .fontWeight(.bold)
                         Text("No requirements.")
-            
                     }
                     .padding()
                     .background(Color.mint.opacity(0.1))
@@ -145,15 +144,15 @@ struct TechView: View {
                 .cornerRadius(10)
                 
                 // Fourth Disclosure Group
-                DisclosureGroup("InspiritAI: Computer Science Summer Program", isExpanded: $isExpanded2) {
+                DisclosureGroup("InspiritAI: Computer Science Summer Program", isExpanded: $isExpanded4) {
                     VStack(alignment: .leading, spacing: 10) {
-                        // Add your content here
                         Text("Overview")
                             .fontWeight(.bold)
-                        
                         Text("Inspirit AI Scholars is a remote computer science summer program that is targeted at students trying to get a preliminary understanding of machine learning.")
+                            .padding()
                             .background(Color.cyan.opacity(0.3))
                             .cornerRadius(10)
+                        
                         Text("Cost")
                             .fontWeight(.bold)
                         Text("$1100 USD")
@@ -164,16 +163,15 @@ struct TechView: View {
                         
                         Text("Who")
                             .fontWeight(.bold)
-                        
                         Text("Grades 9-12.")
                             .fontWeight(.light)
                             .padding()
                             .background(Color.cyan.opacity(0.3))
                             .cornerRadius(10)
+                        
                         Text("Requirements")
                             .fontWeight(.bold)
                         Text("No requirements, but an interest in AI is appreciated.")
-                        
                     }
                     .padding()
                     .background(Color.cyan.opacity(0.1))
@@ -186,14 +184,13 @@ struct TechView: View {
                 // Fifth Disclosure Group
                 DisclosureGroup("Microsoft Discovery Program", isExpanded: $isExpanded5) {
                     VStack(alignment: .leading, spacing: 10) {
-                        // Add your content here
                         Text("Overview")
                             .fontWeight(.bold)
-                        
                         Text("The Microsoft Discovery Program is open to graduating seniors interested in technology who live within 50 miles of Redmond, Washington, or attend high school around Atlanta, Georgia. Participants will learn the fundamentals of programming and get firsthand experience working at one of the world’s largest technology companies. Students will also receive mentorship from Microsoft employees and build valuable career skills.")
-                        
+                            .padding()
                             .background(Color.cyan.opacity(0.3))
                             .cornerRadius(10)
+                        
                         Text("Cost")
                             .fontWeight(.bold)
                         Text("FREE")
@@ -204,16 +201,15 @@ struct TechView: View {
                         
                         Text("Who")
                             .fontWeight(.bold)
-                        
-                        Text("Rising College Freshman")
+                        Text("Rising College Freshmen")
                             .fontWeight(.light)
                             .padding()
                             .background(Color.cyan.opacity(0.3))
                             .cornerRadius(10)
+                        
                         Text("Requirements")
                             .fontWeight(.bold)
                         Text("A free response essay and information on the college you're attending in the fall.")
- 
                     }
                     .padding()
                     .background(Color.cyan.opacity(0.1))
@@ -222,10 +218,7 @@ struct TechView: View {
                 .padding()
                 .background(Color.cyan.opacity(0.3))
                 .cornerRadius(10)
-                
-                Spacer()
             }
-            
             .padding()
         }
     }
@@ -236,5 +229,3 @@ struct TechView_Previews: PreviewProvider {
         TechView()
     }
 }
-
-
