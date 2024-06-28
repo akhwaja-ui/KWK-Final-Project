7cc15f40443533f2766910415256a1de5f06b4d2
Aria Khwaja <ariakhwaja11@gmail.com>
2024-06-28 11:32:40 -0400

    hi

--- KWK-Final Project/ContentView.swift
+++ KWK-Final Project/ContentView.swift
@@ -9,72 +9,70 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-
-            NavigationView {
-                VStack {
-                    ScrollView {
-                        VStack(spacing: 20) {
-                            NavigationLink(destination: ScienceView()) {
-                                VStack {
-                                    Image("folder_icon")
-                                        .resizable()
-                                        .frame(width: 100, height: 100)
-                                        .aspectRatio(contentMode: .fit)
-                                    Text("Science")
-                                }
-                                .padding()
-                                .background(Color.gray.opacity(0.2))
-                                .cornerRadius(10)
-                            }
-
-                            NavigationLink(destination: TechView()) {
-                                VStack {
-                                    Image("folder_icon")
-                                        .resizable()
-                                        .frame(width: 100, height: 100)
-                                        .aspectRatio(contentMode: .fit)
-                                    Text("Technology")
-                                }
-                                .padding()
-                                .background(Color.gray.opacity(0.2))
-                                .cornerRadius(10)
-                            }
-
-                            NavigationLink(destination: EngineerView()) {
-                                VStack {
-                                    Image("folder_icon")
-                                        .resizable()
-                                        .frame(width: 100, height: 100)
-                                        .aspectRatio(contentMode: .fit)
-                                    Text("Engineering")
-                                }
-                                .padding()
-                                .background(Color.gray.opacity(0.2))
-                                .cornerRadius(10)
-                            }
-
-                            NavigationLink(destination: MathView()) {
-                                VStack {
-                                    Image("folder_icon")
-                                        .resizable()
-                                        .frame(width: 100, height: 100)
-                                        .aspectRatio(contentMode: .fit)
-                                    Text("Math")
-                                }
-                                .padding()
-                                .background(Color.gray.opacity(0.2))
-                                .cornerRadius(10)
-                            }
+        NavigationStack {
+            ZStack() {
+                Color(red: 231/255, green: 198/255, blue: 255/255)
+                VStack(spacing:20) {
+                    NavigationLink(destination: ScienceView()) {
+                        VStack {
+                            Image("folder_icon")
+                                .resizable()
+                                .frame(width: 100, height: 100)
+                                .aspectRatio(contentMode: .fit)
+                            Text("Science")
+                            
                         }
                         .padding()
+                        
+                    }
+                    
+                    NavigationLink(destination: TechView()) {
+                        VStack {
+                            Image("folder_icon")
+                                .resizable()
+                                .frame(width: 100, height: 100)
+                                .aspectRatio(contentMode: .fit)
+                            Text("Technology")
+                        }
+                        .padding()
+                        
+                    }
+                    
+                    NavigationLink(destination: EngineerView()) {
+                        VStack {
+                            Image("folder_icon")
+                                .resizable()
+                                .frame(width: 100, height: 100)
+                                .aspectRatio(contentMode: .fit)
+                            Text("Engineering")
+                        }
+                        .padding()
+                        
+                    }
+                    
+                    NavigationLink(destination: MathView()) {
+                        VStack {
+                            Image("folder_icon")
+                                .resizable()
+                                .frame(width: 100, height: 100)
+                                .aspectRatio(contentMode: .fit)
+                            Text("Math")
+                        }
+                        .padding()
+                        
                     }
                 }
+                
+                .padding()
+                
                 .navigationTitle("Home")
                 .navigationBarTitleDisplayMode(.inline)
                 .navigationBarHidden(true)
+                .ignoresSafeArea()
             }
+            .ignoresSafeArea()
         }
+        
     }
 }
 
